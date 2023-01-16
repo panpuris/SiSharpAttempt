83af8a7f03e97f0f2d978fa01806f3c2ebcfdd0c
Vitalii Puris <purisvit@mail.ru>
2023-01-16 18:04:36 +0200

    Add folder examples_method_array

---
+++ examples_method_array/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1,7,2,3,7,4,5,6,7,8};
+
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+//index = index + 1
+    index ++;
+}
