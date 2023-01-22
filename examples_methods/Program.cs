6d39b0bb78005882d059cc57715c996be3b0faed
Vitalii Puris <purisvit@mail.ru>
2023-01-22 13:49:51 +0200

    Add folder examples_methods

---
+++ examples_methods/Program.cs
@@ -0,0 +1,51 @@
+﻿//Виды методов
+//Тип 1
+void Method1()
+{
+    System.Console.WriteLine("Автор ...");
+}
+//Тип 2
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        System.Console.WriteLine(msg);
+        i++;
+    }
+
+
+}
+Method21(msg: "Текст", count: 4);
+
+
+//Тип 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+System.Console.WriteLine(year);
+
+//Тип 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+
+    }
+    return result;
+
+}
+string res = Method4(10, "asdf");
+System.Console.WriteLine(res);
