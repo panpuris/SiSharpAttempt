e2edf0788da0bd33f89f4189d1e2f45f553e094c
Vitalii Puris <purisvit@mail.ru>
2023-01-31 16:39:58 +0200

    Add examples with factorial

---
+++ examples_factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int Factorial(int n)//Для больших чисел необходимо использовать тип данных double 
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+System.Console.WriteLine(Factorial(8));
+
+for ( int i = 1; i < 20; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
