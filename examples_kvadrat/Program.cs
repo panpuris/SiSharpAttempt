d974d9d67b7077a199f8a514b13dceb7cf093596
Vitalii Puris <purisvit@mail.ru>
2023-02-06 22:49:05 +0200

    Add folder examples kvadrat

---
+++ examples_kvadrat/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] pic = new int[10, 10];
+for (int i = 0; i < 10; i++)
+{
+    pic[0, i] = 1;
+    pic[i, 0] = 1;
+    pic[i, 9] = 1;
+    pic[9, i] = 1;
+
+}
+
+PrintImage(pic);
+FillImage(1,1);
+System.Console.WriteLine();
+PrintImage(pic);
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            // System.Console.Write($"{image[i, j]} ");
+            if (image[i, j] == 0) System.Console.Write($" ");
+            else System.Console.Write($"+");
+
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+
+
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row + 1, col);
+        FillImage(row, col - 1);
+        FillImage(row, col + 1);
+
+    }
+}
